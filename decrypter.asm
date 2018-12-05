947812ce14e66ecf6984b936b99db0b007e58397
keenanmach <kmach@ucsd.edu>
2018-12-05 05:46:25 -0800

    updated assembly code for new branch behavior

--- decrypter.asm
+++ decrypter.asm
@@ -1,3 +1,4 @@
+// store the eight lfsr patterns into  memory positions 55-62
 store_lsfr:
 ldr r3, 55 // r3 = mem = 55
 ldr r4, 44 // r4 = 0010 1100
@@ -30,157 +31,134 @@ str r4, r3 // [62] = 0xfa
 ldr r3, 55 // r3 = idx = 54
 // loop to check each lfsr pattern
 find_lfsr:
-	addi r3, 1 // idx = idx + 1 (starts at 55)
-ldr r6, 64 // r6 = encrypted_char = 64
-ldm r4, r3 // r4 = [idx] = lfsr
-ldm r5, r6 // r6 = [encyrpted_char] = state
-ldi 0x20 // lr = space
-xor r5, lr // r5 = state ^ space
-addi r6, 1 // r6 = encrypted_char + 1
+	addi r3, 1 // idx = idx + 1 (starts at 55), check next lfsr state
+	ldr r6, 64 // r6 = encrypted_char = 64
+	ldm r4, r3 // r4 = [idx] = lfsr
+	ldm r5, r6 // r5 = [encrypted_char] = state
+	ldi 0x20 // lr = space
+	xor r5, lr // r5 = state ^ space
+	addi r6, 1 // r6 = encrypted_char + 1
 // loop to check the state eight times
 check_spaces:
-// getLSB, stores in r0, r7, lr assumes r4 = lfsr, r5 = state
-ldr r0, 1
-lsl r0, 5
-lsl r0, 2 // r0 = parity = 1000 0000
-mov r7, r4
-and r7, r0 // r7 = lfsr & 1000 0000
-and r7, r5 // r7 = lfsr & 1000 0000 & x
-xor r0, r7 // r0 = r0 ^ r7
-ldi 1
-lsl lr, 5
+	// getLSB - stores in r0, lr, assumes r4 = lfsr, r5 = state
+	ldr r0, 1 // r0 = parity = 0000 0001
+	ldi 1 // lr = 0000 0001
+	and lr, r4 // lr = lfsr & 0000 0001
+	and lr, r5 // lr = lfsr & 0000 0001 & x
+	xor r0, lr // r0 = parity ^ (lfsr & 0000 0001 & x)
+	ldi 2 // lr = 0000 0010
+	and lr, r5 // lr = 0000 0010 & x
+	and lr, r4 // lr = 0000 0010 & x & lfsr
+	lsr lr, 1 // lr = (0000 0010 & x & lfsr) >> 1
+	xor r0, lr // r0 = parity ^ ((0000 0010 & x & lfsr) >> 1)
+	ldi 4 // lr = 0000 0100
+	and lr, r5 // lr = 0000 0100 & x
+	and lr, r4 // lr = 0000 0100 & x & lfsr
+	lsr lr, 2 // lr = (0000 0100 & x & lfsr) >> 2
+	xor r0, lr // r0 = parity ^ ((0000 0100 & x & lfsr) >> 2)
+ldi 8 // lr = 0000 100
+	and lr, r5 // lr = 0000 1000 & x
+	and lr, r4 // lr = 0000 1000 & x & lfsr
+	lsr lr, 3 // lr = (0000 1000 & x & lfsr) >> 3
+	xor r0, lr // r0 = parity ^ ((0000 1000 & x & lfsr) >> 3)
+ldi 16 // lr = 0001 0000
+	and lr, r5 // lr = 0001 0000 & x
+	and lr, r4 // lr = 0001 0000 & x & lfsr
+	lsr lr, 4 // lr = (0001 0000 & x & lfsr) >> 4
+	xor r0, lr // r0 = parity ^ ((0000 0100 & x & lfsr) >> 4)
+ldi 32 // lr = 0010 0000
+	and lr, r5 // lr = 0010 0000 & x
+	and lr, r4 // lr = 0010 0000 & x & lfsr
+	lsr lr, 5 // lr = (0010 0000 & x & lfsr) >> 5
+	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 5)
+ldi 32 // lr = 0010 0000
 lsl lr, 1 // lr = 0100 0000
-and lr, r5 // lr = x & 0100 0000
-and lr, r4 // lr = lfsr & 0100 0000 & x
-lsl lr, 1 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 5 // lr = 0010 0000
-and lr, r5 // lr = x & 0010 0000
-and lr, r4 // lr = lfsr & 0010 0000 & x
-lsl lr, 2 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 4 // lr = 0001 0000
-and lr, r5 // lr = x & 0001 0000
-and lr, r4 // lr = lfsr & 0001 0000 & x
-lsl lr, 3 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 3
-and lr, r5
-and lr, r4
-lsl lr, 4
-xor r0, lr
-ldi 1
-lsl lr, 2
-and lr, r5
-and lr, r4
-lsl lr, 5
-xor r0, lr
-ldi 1
-lsl lr, 1
-and lr, r5
-and lr, r4
-lsl lr, 5
-lsl lr, 1
-xor r0, lr
-ldi 1 // lr = 0000 0001
-and lr, r5 // lr = x & 0000 0001
-and lr, r4 // lr = x & 0000 0001 & lfsr
-lsl lr, 5
-lsl lr, 2 // lr = _000 0000
-xor r0, lr // r0 = 0 if odd parity, 1 if even parity
-ldr r2, even1 // r2 = even
-ldi 0
-lsl r5, 1 // r5 = x << 1
-bne r0, lr // branch if even parity
-addi r5, 1 // add one if odd parity
-even1:
-// end of getLSB, r5 = new lfsr state
-ldm r7, r6 // r7 = [encrypted_char] = mem_state
-ldi 0x20 // lr = space
-xor r7, lr // r7 = mem_state ^ space
-ldi 1 // lr = 0000 0001
-mov r0, r5 // r0 = state
-and r0, lr // r0 = state lsb
-and r7, lr // r7 = mem_state lsb
-ldr r2, find_lfsr
-bne r0, r2 // if lsbs are not the same check next lfsr state
-addi r6, 1 // r6 = encrypted_char + 1
-ldr r0, 63 // r0 = 63
-ldi 10 // lr = 10
-add r0, lr // r0 = 73
-ldr r2, check_spaces
-bne r6, r0 // while(encrypted_char < 73) check_spaces
-// if reach here state found, r5 = state, r4 = lfsr
+	and lr, r5 // lr = 0100 0000 & x
+	and lr, r4 // lr = 0100 0000 & x & lfsr
+	lsr lr, 5 // lr = (0100 0000 & x & lfsr) >> 6
+	lsr lr, 1
+	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 6)
+ldi 32 // lr = 0010 0000
+lsl lr, 2 // lr = 1000 0000
+	and lr, r5 // lr = 1000 0000 & x
+	and lr, r4 // lr = 1000 0000 & x & lfsr
+	lsr lr, 5 // lr = (1000 0000 & x & lfsr) >> 7
+	lsr lr, 2
+	xor r0, lr // r0 = parity ^ ((1000 0000 x & lfsr) >> 7)
+	// r0 = getLSB(lfsr=r4, state=r5)
+	lsl r5, 1 // r5 = x << 1
+	add r5, r0 // r5 = (x << 1) + getLSB(lfsr=r4, state=r5)
+	ldm r7, r6 // r7 = [encrypted_char] = mem_state
+	ldi 0x20 // lr = space
+	xor r7, lr // r7 = mem_state ^ space
+	ldi 1 // lr = 0000 0001
+	mov r0, r5 // r0 = state
+	and r0, lr // r0 = state lsb
+	and r7, lr // r7 = mem_state lsb
+	ldr r2, find_lfsr
+	bne r0, r7 // if lsbs are not the same check next lfsr state
+	// else continue
+	addi r6, 1 // r6 = encrypted_char + 1
+	ldr r0, 73 // r0 = 73
+	ldr r2, check_spaces
+	bne r6, r0 // while(encrypted_char < 73) check_spaces
+	// if reach here state found, r5=state, r4=lfsr
+	ldz r3 // idx = 0
 decrypt_rest:
-	// getLSB, stores in r0, r7, lr assumes r4 = lfsr, r5 = state
-ldr r3, 0 // idx = 0
-lsl r0, 5
-lsl r0, 2 // r0 = parity = 1000 0000
-mov r7, r4
-and r7, r0 // r7 = lfsr & 1000 0000
-and r7, r5 // r7 = lfsr & 1000 0000 & x
-xor r0, r7 // r0 = r0 ^ r7
-ldi 1
-lsl lr, 5
+	// getLSB - stores in r0, lr, assumes r4 = lfsr, r5 = state
+	ldr r0, 1 // r0 = parity = 0000 0001
+	ldi 1 // lr = 0000 0001
+	and lr, r4 // lr = lfsr & 0000 0001
+	and lr, r5 // lr = lfsr & 0000 0001 & x
+	xor r0, lr // r0 = parity ^ (lfsr & 0000 0001 & x)
+	ldi 2 // lr = 0000 0010
+	and lr, r5 // lr = 0000 0010 & x
+	and lr, r4 // lr = 0000 0010 & x & lfsr
+	lsr lr, 1 // lr = (0000 0010 & x & lfsr) >> 1
+	xor r0, lr // r0 = parity ^ ((0000 0010 & x & lfsr) >> 1)
+	ldi 4 // lr = 0000 0100
+	and lr, r5 // lr = 0000 0100 & x
+	and lr, r4 // lr = 0000 0100 & x & lfsr
+	lsr lr, 2 // lr = (0000 0100 & x & lfsr) >> 2
+	xor r0, lr // r0 = parity ^ ((0000 0100 & x & lfsr) >> 2)
+ldi 8 // lr = 0000 100
+	and lr, r5 // lr = 0000 1000 & x
+	and lr, r4 // lr = 0000 1000 & x & lfsr
+	lsr lr, 3 // lr = (0000 1000 & x & lfsr) >> 3
+	xor r0, lr // r0 = parity ^ ((0000 1000 & x & lfsr) >> 3)
+ldi 16 // lr = 0001 0000
+	and lr, r5 // lr = 0001 0000 & x
+	and lr, r4 // lr = 0001 0000 & x & lfsr
+	lsr lr, 4 // lr = (0001 0000 & x & lfsr) >> 4
+	xor r0, lr // r0 = parity ^ ((0000 0100 & x & lfsr) >> 4)
+ldi 32 // lr = 0010 0000
+	and lr, r5 // lr = 0010 0000 & x
+	and lr, r4 // lr = 0010 0000 & x & lfsr
+	lsr lr, 5 // lr = (0010 0000 & x & lfsr) >> 5
+	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 5)
+ldi 32 // lr = 0010 0000
 lsl lr, 1 // lr = 0100 0000
-and lr, r5 // lr = x & 0100 0000
-and lr, r4 // lr = lfsr & 0100 0000 & x
-lsl lr, 1 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 5 // lr = 0010 0000
-and lr, r5 // lr = x & 0010 0000
-and lr, r4 // lr = lfsr & 0010 0000 & x
-lsl lr, 2 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 4 // lr = 0001 0000
-and lr, r5 // lr = x & 0001 0000
-and lr, r4 // lr = lfsr & 0001 0000 & x
-lsl lr, 3 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 3
-and lr, r5
-and lr, r4
-lsl lr, 4
-xor r0, lr
-ldi 1
-lsl lr, 2
-and lr, r5
-and lr, r4
-lsl lr, 5
-xor r0, lr
-ldi 1
-lsl lr, 1
-and lr, r5
-and lr, r4
-lsl lr, 5
-lsl lr, 1
-xor r0, lr
-ldi 1 // lr = 0000 0001
-and lr, r5 // lr = x & 0000 0001
-and lr, r4 // lr = x & 0000 0001 & lfsr
-lsl lr, 7 // lr = _000 0000
-xor r0, lr // r0 = 0 if odd parity, 1 if even parity
-ldr r2, even2 // r2 = even
-ldi 0
-lsl r5, 1 // r5 = x << 1
-bne r0, lr // branch if even parity
-addi r5, 1 // add one if odd parity
-even2:
-// end of getLSB, r5 = new lfsr state
+	and lr, r5 // lr = 0100 0000 & x
+	and lr, r4 // lr = 0100 0000 & x & lfsr
+	lsr lr, 5 // lr = (0100 0000 & x & lfsr) >> 6
+	lsr lr, 1
+	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 6)
+ldi 32 // lr = 0010 0000
+lsl lr, 2 // lr = 1000 0000
+	and lr, r5 // lr = 1000 0000 & x
+	and lr, r4 // lr = 1000 0000 & x & lfsr
+	lsr lr, 5 // lr = (1000 0000 & x & lfsr) >> 7
+	lsr lr, 2
+	xor r0, lr // r0 = parity ^ ((1000 0000 x & lfsr) >> 7)
+	// r0 = getLSB(lfsr=r4, state=r5)
+	lsl r5, 1 // r5 = x << 1
+	add r5, r0 // r5 = (x << 1) + getLSB(lfsr=r4, state=r5)
 	ldm r7, r6 // r7 = [encrypted_char]
 	xor r7, r5 // r7 = [encrypted_char] ^ state
-str r7, r3 // [idx] = [encrypted_char] ^ state = decrypted_char
-addi r3, 1 // idx = idx + 1
-addi r6, 1 // encrypted_char = encrypted_char + 1 
+	str r7, r3 // [idx] = [encrypted_char] ^ state = decrypted_char
+	addi r3, 1 // idx = idx + 1
+	addi r6, 1 // encrypted_char = encrypted_char + 1
+	ldr r7, 128
 	ldr r2, decrypt_rest
-	ldi 1
-	lsl lr, 5
-	lsl lr, 2
-	bne lr, r6 // while(encrypted_char < 128) decrypt_rest
+	bne r7, r6 // while(encrypted_char < 128) decrypt_rest
 	bne r7, r7 // halt
--- encrypter.asm
+++ encrypter.asm
@@ -1,141 +1,124 @@
 encrypt:
 ldi 62
-ldm r4, lr // r4 = [62]  = lfsr
+ldm r4, lr // r4 = [62]  = lfsr_pattern
 ldi 63
 ldm r5, lr // r5 = [63] = lfsr_state = x
 ldz r6 // r6 = curr_mem = 0
 prepend_spaces:
-ldr r7, 0x20
-xor r7, r4 // r7 = space ^ x
-mov r0, r6
-addi r0, 64 // r0 = curr_mem + 64
-str r7, r0 // [r0] = space ^ x
-// getLSB stores in r0, r7, lr assumes r4 = lfsr, r5 = state
-ldr r0, 1
-lsl r0, 5
-lsl r0, 2 // r0 = parity = 1000 0000
-mov r7, r4
-and r7, r0 // r7 = lfsr & 1000 0000
-and r7, r5 // r7 = lfsr & 1000 0000 & x
-xor r0, r7 // r0 = r0 ^ r7
-ldi 1
-lsl lr, 5
+	ldr r7, 0x20
+	xor r7, r4 // r7 = space ^ x
+	mov r0, r6
+	addi r0, 64 // r0 = curr_mem + 64
+	str r7, r0 // [r0] = space ^ x
+	// getLSB - stores in r0, lr, assumes r4 = lfsr, r5 = state
+	ldr r0, 1 // r0 = parity = 0000 0001
+	ldi 1 // lr = 0000 0001
+	and lr, r4 // lr = lfsr & 0000 0001
+	and lr, r5 // lr = lfsr & 0000 0001 & x
+	xor r0, lr // r0 = parity ^ (lfsr & 0000 0001 & x)
+	ldi 2 // lr = 0000 0010
+	and lr, r5 // lr = 0000 0010 & x
+	and lr, r4 // lr = 0000 0010 & x & lfsr
+	lsr lr, 1 // lr = (0000 0010 & x & lfsr) >> 1
+	xor r0, lr // r0 = parity ^ ((0000 0010 & x & lfsr) >> 1)
+	ldi 4 // lr = 0000 0100
+	and lr, r5 // lr = 0000 0100 & x
+	and lr, r4 // lr = 0000 0100 & x & lfsr
+	lsr lr, 2 // lr = (0000 0100 & x & lfsr) >> 2
+	xor r0, lr // r0 = parity ^ ((0000 0100 & x & lfsr) >> 2)
+ldi 8 // lr = 0000 100
+	and lr, r5 // lr = 0000 1000 & x
+	and lr, r4 // lr = 0000 1000 & x & lfsr
+	lsr lr, 3 // lr = (0000 1000 & x & lfsr) >> 3
+	xor r0, lr // r0 = parity ^ ((0000 1000 & x & lfsr) >> 3)
+ldi 16 // lr = 0001 0000
+	and lr, r5 // lr = 0001 0000 & x
+	and lr, r4 // lr = 0001 0000 & x & lfsr
+	lsr lr, 4 // lr = (0001 0000 & x & lfsr) >> 4
+	xor r0, lr // r0 = parity ^ ((0000 0100 & x & lfsr) >> 4)
+ldi 32 // lr = 0010 0000
+	and lr, r5 // lr = 0010 0000 & x
+	and lr, r4 // lr = 0010 0000 & x & lfsr
+	lsr lr, 5 // lr = (0010 0000 & x & lfsr) >> 5
+	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 5)
+ldi 32 // lr = 0010 0000
 lsl lr, 1 // lr = 0100 0000
-and lr, r5 // lr = x & 0100 0000
-and lr, r4 // lr = lfsr & 0100 0000 & x
-lsl lr, 1 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 5 // lr = 0010 0000
-and lr, r5 // lr = x & 0010 0000
-and lr, r4 // lr = lfsr & 0010 0000 & x
-lsl lr, 2 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 4 // lr = 0001 0000
-and lr, r5 // lr = x & 0001 0000
-and lr, r4 // lr = lfsr & 0001 0000 & x
-lsl lr, 3 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 3
-and lr, r5
-and lr, r4
-lsl lr, 4
-xor r0, lr
-ldi 1
-lsl lr, 2
-and lr, r5
-and lr, r4
-lsl lr, 5
-xor r0, lr
-ldif 1
-lsl lr, 1
-and lr, r5
-and lr, r4
-lsl lr, 5
-lsl lr, 1
-xor r0, lr
-ld 1 // lr = 0000 0001
-and lr, r5 // lr = x & 0000 0001
-and lr, r4 // lr = x & 0000 0001 & lfsr
-lsl lr, 5 // lr = _000 0000
-lsl lr, 2
-xor r0, lr // r0 = 0 if odd parity, 1 if even parity
-ldr r2, even1 // r2 = even
-ldi 0
-lsl r5, 1 // r5 = x << 1
-bne r0, lr // branch if even parity
-addi r5, 1 // add one if odd parity
-even1:
-// end of getLSB
-addi r6, 1 // curr_mem = curr_mem + 1
-ldr r2, prepend_spaces
-ldi 62
-ldm lr, lr // lr = [62] = num_spaces
-bne r6, lr // while(curr_mem < num_spaces)
-// get LSB for rest of string (not spaces)
+	and lr, r5 // lr = 0100 0000 & x
+	and lr, r4 // lr = 0100 0000 & x & lfsr
+	lsr lr, 5 // lr = (0100 0000 & x & lfsr) >> 6
+	lsr lr, 1
+	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 6)
+ldi 32 // lr = 0010 0000
+lsl lr, 2 // lr = 1000 0000
+	and lr, r5 // lr = 1000 0000 & x
+	and lr, r4 // lr = 1000 0000 & x & lfsr
+	lsr lr, 5 // lr = (1000 0000 & x & lfsr) >> 7
+	lsr lr, 2
+	xor r0, lr // r0 = parity ^ ((1000 0000 x & lfsr) >> 7)
+	// r0 = getLSB(lfsr=r4, state=r5)
+	lsl r5, 1 // r5 = x << 1
+	add r5, r0 // r5 = (x << 1) + getLSB(lfsr=r4, state=r5)
+	addi r6, 1 // curr_mem = curr_mem + 1
+	ldr r3, 61
+	ldm r3, r3 // r3 = [61] = num_spaces
+	ldr r2, prepend_spaces // lr = prepend_spaces
+	bne r6, r3 // while(curr_mem < num_spaces) prepend_spaces
 encrypt_char:
-ldr r0, 1
-lsl r0, 5
-lsl r0, 2 // r0 = parity = 1000 0000
-mov r7, r4
-and r7, r0 // r7 = lfsr & 1000 0000
-and r7, r5 // r7 = lfsr & 1000 0000 & x
-xor r0, r7 // r0 = r0 ^ r7
-ldi 1
-lsl lr, 5
+	ldm r7, r6 // r7 = [curr_mem] = char
+	xor r7, r5 // r7 = char ^ x
+	mov r2, r6 // r2 = curr_mem
+	addi r2, 64 // r2 = curr_mem + 64
+	str r7, r2 // [curr_mem + 64] = char ^ x
+// getLSB - stores in r0, lr, assumes r4 = lfsr, r5 = state
+	ldr r0, 1 // r0 = parity = 0000 0001
+	ldi 1 // lr = 0000 0001
+	and lr, r4 // lr = lfsr & 0000 0001
+	and lr, r5 // lr = lfsr & 0000 0001 & x
+	xor r0, lr // r0 = parity ^ (lfsr & 0000 0001 & x)
+	ldi 2 // lr = 0000 0010
+	and lr, r5 // lr = 0000 0010 & x
+	and lr, r4 // lr = 0000 0010 & x & lfsr
+	lsr lr, 1 // lr = (0000 0010 & x & lfsr) >> 1
+	xor r0, lr // r0 = parity ^ ((0000 0010 & x & lfsr) >> 1)
+	ldi 4 // lr = 0000 0100
+	and lr, r5 // lr = 0000 0100 & x
+	and lr, r4 // lr = 0000 0100 & x & lfsr
+	lsr lr, 2 // lr = (0000 0100 & x & lfsr) >> 2
+	xor r0, lr // r0 = parity ^ ((0000 0100 & x & lfsr) >> 2)
+ldi 8 // lr = 0000 100
+	and lr, r5 // lr = 0000 1000 & x
+	and lr, r4 // lr = 0000 1000 & x & lfsr
+	lsr lr, 3 // lr = (0000 1000 & x & lfsr) >> 3
+	xor r0, lr // r0 = parity ^ ((0000 1000 & x & lfsr) >> 3)
+ldi 16 // lr = 0001 0000
+	and lr, r5 // lr = 0001 0000 & x
+	and lr, r4 // lr = 0001 0000 & x & lfsr
+	lsr lr, 4 // lr = (0001 0000 & x & lfsr) >> 4
+	xor r0, lr // r0 = parity ^ ((0000 0100 & x & lfsr) >> 4)
+ldi 32 // lr = 0010 0000
+	and lr, r5 // lr = 0010 0000 & x
+	and lr, r4 // lr = 0010 0000 & x & lfsr
+	lsr lr, 5 // lr = (0010 0000 & x & lfsr) >> 5
+	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 5)
+ldi 32 // lr = 0010 0000
 lsl lr, 1 // lr = 0100 0000
-and lr, r5 // lr = x & 0100 0000
-and lr, r4 // lr = lfsr & 0100 0000 & x
-lsl lr, 1 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 5 // lr = 0010 0000
-and lr, r5 // lr = x & 0010 0000
-and lr, r4 // lr = lfsr & 0010 0000 & x
-lsl lr, 2 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 4 // lr = 0001 0000
-and lr, r5 // lr = x & 0001 0000
-and lr, r4 // lr = lfsr & 0001 0000 & x
-lsl lr, 3 // lr = _000 0000
-xor r0, lr // r0 = parity ^ _000 0000
-ldi 1
-lsl lr, 3
-and lr, r5
-and lr, r4
-lsl lr, 4
-xor r0, lr
-ldi 1
-lsl lr, 2
-and lr, r5
-and lr, r4
-lsl lr, 5
-xor r0, lr
-ldi 1
-lsl lr, 1
-and lr, r5
-and lr, r4
-lsl lr, 5
-lsl lr, 1
-xor r0, lr
-ldi 1 // lr = 0000 0001
-and lr, r5 // lr = x & 0000 0001
-and lr, r4 // lr = x & 0000 0001 & lfsr
-lsl lr, 5
-lsl lr, 2 // lr = _000 0000
-xor r0, lr // r0 = 0 if odd parity, 1 if even parity
-ldr r2, even2 // r2 = even
-ldi 0
-lsl r5, 1 // r5 = x << 1
-bne r0, lr // branch if even parity
-addi r5, 1 // add one if odd parity
-even2:
-// end of getLSB for rest of string
-addi r6, 1 // curr_mem = curr_mem + 1
-ldr r2, encrypt_char
-ldi 64
-bne r6, lr // while(curr_mem < 64)
-bne r7, r7 // halt
+	and lr, r5 // lr = 0100 0000 & x
+	and lr, r4 // lr = 0100 0000 & x & lfsr
+	lsr lr, 5 // lr = (0100 0000 & x & lfsr) >> 6
+	lsr lr, 1
+	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 6)
+ldi 32 // lr = 0010 0000
+lsl lr, 2 // lr = 1000 0000
+	and lr, r5 // lr = 1000 0000 & x
+	and lr, r4 // lr = 1000 0000 & x & lfsr
+	lsr lr, 5 // lr = (1000 0000 & x & lfsr) >> 7
+	lsr lr, 2
+	xor r0, lr // r0 = parity ^ ((1000 0000 x & lfsr) >> 7)
+	// r0 = getLSB(lfsr=r4, state=r5)
+	lsl r5, 1 // r5 = x << 1
+	add r5, r0 // r5 = (x << 1) + getLSB(lfsr=r4, state=r5)
+	addi r6, 1 // curr_mem = curr_mem + 1
+	ldr r7, 64 // r7 = 64
+	ldr r2, encrypt_char // lr = encrypt_char
+	bne r6, r7 // while(curr_mem < 64) encrypt_char
+	bne r7, r7 // halt
