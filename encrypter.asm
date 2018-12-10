067f17f94b58968c1b5b054e7ddc1c609ed04d60
keenanmach <kmach@ucsd.edu>
2018-12-10 03:53:57 -0800

    encrypter passes provided testbench

--- encrypter.asm
+++ encrypter.asm
@@ -6,12 +6,12 @@ ldm r5, lr // r5 = [63] = lfsr_state = x
 ldz r6 // r6 = curr_mem = 0
 prepend_spaces:
 	ldr r7, 0x20
-	xor r7, r4 // r7 = space ^ x
+	xor r7, r5 // r7 = space ^ x
 	mov r0, r6
 	addi r0, 64 // r0 = curr_mem + 64
 	str r7, r0 // [r0] = space ^ x
 	// getLSB - stores in r0, lr, assumes r4 = lfsr, r5 = state
-	ldr r0, 1 // r0 = parity = 0000 0001
+	ldz r0 // r0 = parity = 0000 0000
 	ldi 1 // lr = 0000 0001
 	and lr, r4 // lr = lfsr & 0000 0001
 	and lr, r5 // lr = lfsr & 0000 0001 & x
@@ -39,21 +39,22 @@ ldi 16 // lr = 0001 0000
 ldi 32 // lr = 0010 0000
 	and lr, r5 // lr = 0010 0000 & x
 	and lr, r4 // lr = 0010 0000 & x & lfsr
-	lsr lr, 5 // lr = (0010 0000 & x & lfsr) >> 5
+	lsr lr, 4 // lr = (0010 0000 & x & lfsr) >> 5
+	lsr lr, 1
 	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 5)
 ldi 32 // lr = 0010 0000
 lsl lr, 1 // lr = 0100 0000
 	and lr, r5 // lr = 0100 0000 & x
 	and lr, r4 // lr = 0100 0000 & x & lfsr
-	lsr lr, 5 // lr = (0100 0000 & x & lfsr) >> 6
-	lsr lr, 1
+	lsr lr, 4 // lr = (0100 0000 & x & lfsr) >> 6
+	lsr lr, 2
 	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 6)
 ldi 32 // lr = 0010 0000
 lsl lr, 2 // lr = 1000 0000
 	and lr, r5 // lr = 1000 0000 & x
 	and lr, r4 // lr = 1000 0000 & x & lfsr
-	lsr lr, 5 // lr = (1000 0000 & x & lfsr) >> 7
-	lsr lr, 2
+	lsr lr, 4 // lr = (1000 0000 & x & lfsr) >> 7
+	lsr lr, 3
 	xor r0, lr // r0 = parity ^ ((1000 0000 x & lfsr) >> 7)
 	// r0 = getLSB(lfsr=r4, state=r5)
 	lsl r5, 1 // r5 = x << 1
@@ -63,14 +64,16 @@ lsl lr, 2 // lr = 1000 0000
 	ldm r3, r3 // r3 = [61] = num_spaces
 	ldr r2, prepend_spaces // lr = prepend_spaces
 	bne r6, r3 // while(curr_mem < num_spaces) prepend_spaces
+	ldz r6 // r6 = curr_mem = 0(start reading from 0)
 encrypt_char:
 	ldm r7, r6 // r7 = [curr_mem] = char
 	xor r7, r5 // r7 = char ^ x
 	mov r2, r6 // r2 = curr_mem
-	addi r2, 64 // r2 = curr_mem + 64
-	str r7, r2 // [curr_mem + 64] = char ^ x
+	add r2, r3 // r2 = curr_mem + num_spaces
+	addi r2, 64 // r2 = curr_mem num_spaces + 64
+	str r7, r2 // [curr_mem + num_spaces + 64] = char ^ x
 // getLSB - stores in r0, lr, assumes r4 = lfsr, r5 = state
-	ldr r0, 1 // r0 = parity = 0000 0001
+	ldz r0 // r0 = parity = 0000 0000
 	ldi 1 // lr = 0000 0001
 	and lr, r4 // lr = lfsr & 0000 0001
 	and lr, r5 // lr = lfsr & 0000 0001 & x
@@ -98,21 +101,22 @@ ldi 16 // lr = 0001 0000
 ldi 32 // lr = 0010 0000
 	and lr, r5 // lr = 0010 0000 & x
 	and lr, r4 // lr = 0010 0000 & x & lfsr
-	lsr lr, 5 // lr = (0010 0000 & x & lfsr) >> 5
+	lsr lr, 4 // lr = (0010 0000 & x & lfsr) >> 5
+	lsr lr, 1
 	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 5)
 ldi 32 // lr = 0010 0000
 lsl lr, 1 // lr = 0100 0000
 	and lr, r5 // lr = 0100 0000 & x
 	and lr, r4 // lr = 0100 0000 & x & lfsr
-	lsr lr, 5 // lr = (0100 0000 & x & lfsr) >> 6
-	lsr lr, 1
+	lsr lr, 4 // lr = (0100 0000 & x & lfsr) >> 6
+	lsr lr, 2
 	xor r0, lr // r0 = parity ^ ((0010 0000 x & lfsr) >> 6)
 ldi 32 // lr = 0010 0000
 lsl lr, 2 // lr = 1000 0000
 	and lr, r5 // lr = 1000 0000 & x
 	and lr, r4 // lr = 1000 0000 & x & lfsr
-	lsr lr, 5 // lr = (1000 0000 & x & lfsr) >> 7
-	lsr lr, 2
+	lsr lr, 4 // lr = (1000 0000 & x & lfsr) >> 7
+	lsr lr, 3
 	xor r0, lr // r0 = parity ^ ((1000 0000 x & lfsr) >> 7)
 	// r0 = getLSB(lfsr=r4, state=r5)
 	lsl r5, 1 // r5 = x << 1
@@ -120,5 +124,5 @@ lsl lr, 2 // lr = 1000 0000
 	addi r6, 1 // curr_mem = curr_mem + 1
 	ldr r7, 64 // r7 = 64
 	ldr r2, encrypt_char // lr = encrypt_char
-	bne r6, r7 // while(curr_mem < 64) encrypt_char
+	bne r6, r7 // while(curr_mem < 40) encrypt_char
 	bne r7, r7 // halt
